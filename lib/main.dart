96ccf701a8cc00825d2a632f506d80e2eed97f75
FlutLab.io <>
2021-05-10 19:51:03 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -77,18 +77,13 @@ class Inicio extends StatelessWidget {
     return new Scaffold(
       body: Container(
         padding: EdgeInsets.only(top: 130, bottom: 10, right: 10, left: 10),
-        decoration: BoxDecoration(
-          color: Colors.lightBlue,
-          image: DecorationImage(
-            image: NetworkImage("https://raw.githubusercontent.com/FaridCG343/MisImagenes/main/consejos-vacunas-peru.png"),
-            alignment: Alignment.topCenter,
-          ),
-        ), //fin de box decoration
+        decoration: BoxDecoration(color: Colors.blueGrey, image: DecorationImage(image: NetworkImage("https://raw.githubusercontent.com/Luhhii/mis_imagenes/main/MicrosoftTeams-image.png"), alignment: Alignment.topCenter)),
         child: Column(
           children: <Widget>[
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-              Column(
-                children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Column(children: <Widget>[
                   Padding(
                     padding: EdgeInsets.all(10),
                     child: RaisedButton(
@@ -101,52 +96,86 @@ class Inicio extends StatelessWidget {
                         width: 100,
                         height: 100,
                         child: Center(
-                          child: Text(
-                            "Inicio",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: Colors.grey,
-                              fontWeight: FontWeight.w900,
-                            ),
+                          child: Text("INICIO", textAlign: TextAlign.center),
+                        ),
+                      ),
+                    ),
+                  ) //fin de padding
+                ] //fin de widget
+                    ), //fin de columun
+                Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: RaisedButton(
+                        color: Colors.white,
+                        shape: new RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/empresa");
+                        },
+                        child: SizedBox(
+                          width: 100,
+                          height: 100,
+                          child: Center(
+                            child: Text("EMPRESA", textAlign: TextAlign.center),
                           ),
-                        ), //center
-                      ), //sized box
-                    ), //boton inicio
-                  ), //padding
-                ], //widget[]
-              ), //fin columna 1
-              Column(
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.all(10),
-                    child: RaisedButton(
-                      color: Colors.white,
-                      shape: new RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-                      onPressed: () {
-                        Navigator.pushNamed(context, "/empresa");
-                      },
-                      child: SizedBox(
-                        width: 100,
-                        height: 100,
-                        child: Center(
-                          child: Text(
-                            "Empresa",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: Colors.grey,
-                              fontWeight: FontWeight.w900,
-                            ),
+                        ),
+                      ),
+                    )
+                  ],
+                ), //r1 columna 2
+              ], //fin de widget
+            ), //fin de fila
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: RaisedButton(
+                        color: Colors.white,
+                        shape: new RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/productos");
+                        },
+                        child: SizedBox(
+                          width: 100,
+                          height: 100,
+                          child: Center(
+                            child: Text("PRODUCTOS", textAlign: TextAlign.center),
                           ),
-                        ), //center
-                      ), //sized box
-                    ), //boton inicio
-                  ), //padding
-                ], //widget[]
-              ), //fin columna 2
-            ]), //fin de fila 1
-          ], //fin de widget []
-        ),
-      ), //fin de container
-    ); // fin de sacffold
-  } //fin de widget build inicio
-} //fin de inicio
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: RaisedButton(
+                        color: Colors.white,
+                        shape: new RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/contacto");
+                        },
+                        child: SizedBox(
+                          width: 100,
+                          height: 100,
+                          child: Center(
+                            child: Text("CONTACTO", textAlign: TextAlign.center),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ], //fin de widget
+        ), //fin de column child //fin de box decoration
+      ), //fin de body center
+    ); //fin de scaffold
+  } //fin de widget
+} //fin de clase inicio
