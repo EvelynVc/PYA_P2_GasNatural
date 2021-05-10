b949be1820ac5a84b09404003556102343f23713
FlutLab.io <>
2021-05-10 23:37:29 +0000

    Switching to branch version3

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Gas Naural',
       theme: ThemeData(
-        primarySwatch: Colors.yellow,
+        primarySwatch: Colors.blueGrey,
       ), //fin de tema
       //ruta de ventanas
       routes: <String, WidgetBuilder>{
         "/inicio": (BuildContext context) => Inicio(),
         "/gasNatural": (BuildContext context) => Gasnatural(),
-        "/servico": (BuildContext context) => Servicio(),
+        "/servicio": (BuildContext context) => Servicio(),
         "/contacto": (BuildContext context) => Contacto(),
       }, //fin de rutas
       home: Inicio(),
@@ -28,7 +29,7 @@ class Gasnatural extends StatelessWidget {
     return new Scaffold(
       appBar: AppBar(
         title: new Text('Gas Natural'),
-        backgroundColor: Colors.blueAccent,
+        backgroundColor: Colors.indigo[900],
         //cambia color xd .............
       ), //fin de AppBar
       body: Center(
@@ -44,7 +45,7 @@ class Servicio extends StatelessWidget {
     return new Scaffold(
       appBar: AppBar(
         title: new Text('Servicio'),
-        backgroundColor: Colors.blueAccent,
+        backgroundColor: Colors.indigo[900],
         //cambia color xd .............
       ), //fin de AppBar
       body: Center(
@@ -60,7 +61,7 @@ class Contacto extends StatelessWidget {
     return new Scaffold(
       appBar: AppBar(
         title: new Text('Contacto'),
-        backgroundColor: Colors.blueAccent,
+        backgroundColor: Colors.indigo[900],
         //cambia color xd .............
       ), //fin de AppBar
       body: Center(
@@ -75,11 +76,121 @@ class Inicio extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Scaffold(
       body: Container(
-        padding: EdgeInsets.only(top: 130, bottom: 10, right: 10, left: 10), //fin de padding
-        decoration: BoxDecoration(
-          color: Colors.blueGrey,
-          image: DecorationImage(image: NetworkImage("https://raw.githubusercontent.com/EvelynVc/mis_imagenes/main/llama.jpg"), alignment: Alignment.topCenter),
-        ), //fin de box decoration
+        padding: EdgeInsets.only(top: 130, bottom: 10, right: 10, left: 10),
+        decoration: BoxDecoration(color: Colors.blueGrey[50], image: DecorationImage(image: NetworkImage("https://raw.githubusercontent.com/EvelynVc/mis_imagenes/main/gas_natural.jpg"), alignment: Alignment.topCenter)),
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Column(children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: RaisedButton(
+                      color: Colors.amber,
+                      shape: new RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                      onPressed: () {
+                        Navigator.pushNamed(context, "/inicio");
+                      },
+                      child: SizedBox(
+                        width: 100,
+                        height: 100,
+                        child: Center(
+                          child: Text(
+                            "INICIO",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 18, color: Colors.indigo[900]),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ) //fin de padding
+                ] //fin de widget
+                    ), //fin de columun
+                Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: RaisedButton(
+                        color: Colors.amber,
+                        shape: new RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/gasNatural");
+                        },
+                        child: SizedBox(
+                          width: 100,
+                          height: 100,
+                          child: Center(
+                            child: Text(
+                              "GAS NATURAL",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontSize: 18, color: Colors.indigo[900]),
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ), //r1 columna 2
+              ], //fin de widget
+            ), //fin de fila
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: RaisedButton(
+                        color: Colors.amber,
+                        shape: new RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/servicio");
+                        },
+                        child: SizedBox(
+                          width: 100,
+                          height: 100,
+                          child: Center(
+                            child: Text(
+                              "SERVICIO",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontSize: 18, color: Colors.indigo[900]),
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: RaisedButton(
+                        color: Colors.amber,
+                        shape: new RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/contacto");
+                        },
+                        child: SizedBox(
+                          width: 100,
+                          height: 100,
+                          child: Center(
+                            child: Text(
+                              "CONTACTO",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontSize: 18, color: Colors.indigo[900]),
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ], //fin de widget
+        ), //fin de column child //fin de box decoration
       ), //fin de body center
     ); //fin de scaffold
   } //fin de widget
