6970aa09c7e0f115c273218112732f7022d9fec1
FlutLab.io <>
2021-05-10 19:21:31 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -83,40 +83,38 @@ class Inicio extends StatelessWidget {
           child: Column(
             children: <Widget>[
               Row(
-                mainAxisAlignment:MainAxisAlignment.center,
-                Column(
-                  children <Widget>[
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Column(children: <Widget>[
                     Padding(
                       padding: EdgeInsets.all(10),
                       child: RaisedButton(
                         color: Colors.amber,
                         shape: new RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(10.0),
-                        ),//fin de rounded
-                        onPressed: (){
-                          Navigator.pushNamed(context,"/inicio"):
-                        }//fin de onpressed
-                        child:SizedBox(
-                        width: 100,
-                        height: 100,
-                        child: Center(
+                        ), //fin de rounded
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/inicio");
+                        }, //fin de onpressed
+                        child: SizedBox(
+                          width: 100,
+                          height: 100,
+                          child: Center(
                             child: Text(
                               'INICIO',
                               textAlign: TextAlign.center,
-                              style: TextStyle(
-                              color: Coolors.red,
-                              fontWeight: FontWeight.w900
-                              ),//fin de TextStyle
-                            ),//fin de text
-                          ),//fin de center
-                        ),//fin de sizedbox
-                      ),//boton inicio
-                    )//fin de padding
-                  ]//fin de widget
-                ),//fin de columun
-              ),//fin de fila
-            ]//fin de widget
-          ),//fin de column child
+                              style: TextStyle(color: Coolors.red, fontWeight: FontWeight.w900), //fin de TextStyle
+                            ), //fin de text
+                          ), //fin de center
+                        ), //fin de sizedbox
+                      ), //boton inicio
+                    ) //fin de padding
+                  ] //fin de widget
+                      ), //fin de columun
+                ], //fin de widget
+              ), //fin de fila
+            ], //fin de widget
+          ), //fin de column child
         ), //fin de box decoration
       ), //fin de body center
     ); //fin de scaffold
