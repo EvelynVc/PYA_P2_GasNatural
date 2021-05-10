cd8716a0c11e3929f967acf3467d41fe83dc71fa
FlutLab.io <>
2021-05-10 19:47:40 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -76,46 +76,77 @@ class Inicio extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Scaffold(
       body: Container(
-        padding: EdgeInsets.only(top: 130, bottom: 10, right: 10, left: 10), //fin de padding
+        padding: EdgeInsets.only(top: 130, bottom: 10, right: 10, left: 10),
         decoration: BoxDecoration(
-          color: Colors.blueGrey[50], image: DecorationImage(image: NetworkImage("https://raw.githubusercontent.com/EvelynVc/mis_imagenes/main/llama.jpg"), alignment: Alignment.topCenter),
-          child: Column(
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+          color: Colors.lightBlue,
+          image: DecorationImage(
+            image: NetworkImage("https://raw.githubusercontent.com/FaridCG343/MisImagenes/main/consejos-vacunas-peru.png"),
+            alignment: Alignment.topCenter,
+          ),
+        ), //fin de box decoration
+        child: Column(
+          children: <Widget>[
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+              Column(
                 children: <Widget>[
-                  Column(children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.all(10),
-                      child: RaisedButton(
-                        color: Colors.amber,
-                        shape: new RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                        ), //fin de rounded
-                        onPressed: () {
-                          Navigator.pushNamed(context, "/inicio");
-                        }, //fin de onpressed
-                        child: SizedBox(
-                          width: 100,
-                          height: 100,
-                          child: Center(
-                            child: Text(
-                              'INICIO',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(color: Coolors.red, fontWeight: FontWeight.w900), //fin de TextStyle
-                            ), //fin de text
-                          ), //fin de center
-                        ), //fin de sizedbox
-                      ), //boton inicio
-                    ) //fin de padding
-                  ] //fin de widget
-                      ), //fin de columun
-                ], //fin de widget
-              ),
-            ], //fin de widged
-          ), //fin de box decoration
-        ), //fin de body center
-      ),
-    ); //fin de scaffold
-  } //fin de widget
-} //fin de clase inicio
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: RaisedButton(
+                      color: Colors.white,
+                      shape: new RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                      onPressed: () {
+                        Navigator.pushNamed(context, "/inicio");
+                      },
+                      child: SizedBox(
+                        width: 100,
+                        height: 100,
+                        child: Center(
+                          child: Text(
+                            "Inicio",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontWeight: FontWeight.w900,
+                            ),
+                          ),
+                        ), //center
+                      ), //sized box
+                    ), //boton inicio
+                  ), //padding
+                ], //widget[]
+              ), //fin columna 1
+              Column(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: RaisedButton(
+                      color: Colors.white,
+                      shape: new RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                      onPressed: () {
+                        Navigator.pushNamed(context, "/empresa");
+                      },
+                      child: SizedBox(
+                        width: 100,
+                        height: 100,
+                        child: Center(
+                          child: Text(
+                            "Empresa",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontWeight: FontWeight.w900,
+                            ),
+                          ),
+                        ), //center
+                      ), //sized box
+                    ), //boton inicio
+                  ), //padding
+                ], //widget[]
+              ), //fin columna 2
+            ]), //fin de fila 1
+          ], //fin de widget []
+        ),
+      ), //fin de container
+    ); // fin de sacffold
+  } //fin de widget build inicio
+} //fin de inicio
